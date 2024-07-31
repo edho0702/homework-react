e2b3093def80b93131019e943c306dcf3488e0da
dongho <ldd0702@naver.com>
2024-07-31 10:56:28 +0900

    [과제] eslint error 수정..

--- vite-custom-template/eslint.config.js
+++ vite-custom-template/eslint.config.js
@@ -1,19 +1,35 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import pluginReact, { rules } from "eslint-plugin-react";
 
 export default [
     {
         files: ["**/*.{js,mjs,cjs,jsx}"],
     },
     {
+        Plugin: {
+            react: pluginReact,
+        },
+    },
+    {
         languageOptions: {
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
             globals: {
                 ...globals.browser,
                 ...globals.node,
             },
         },
     },
+    {
+        rules: {
+            "react/reat-in-jsx-scope": "off",
+        },
+    },
+
     pluginJs.configs.recommended,
     pluginReact.configs.flat.recommended,
 ];

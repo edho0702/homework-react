9e1155402506c6c60cdddf58fde505e32d1e634b
dongho <ldd0702@naver.com>
2024-08-12 00:32:11 +0900

    [과제] Tic-Tec-Toe
    - 플레이어 대상과 승리 대상 구현

--- Tic-Tec-Toe/src/component/Status/Status.jsx
+++ Tic-Tec-Toe/src/component/Status/Status.jsx
@@ -1,3 +1,14 @@
-export default function Status() {
-    return <h2 className="Status">player : 🐮</h2>;
+import { OneOfPlayerType } from "@/types/type.d";
+import S from "./Status.module.css";
+
+Status.propTypes = {
+    winner: OneOfPlayerType,
+    next: OneOfPlayerType.isRequired,
+};
+
+export default function Status({ winner, next }) {
+    if (winner) {
+        return <h2 className={S.winner}>Winner : {winner}</h2>;
+    }
+    return <h2 className={S.component}>Player : {next}</h2>;
 }
--- Tic-Tec-Toe/src/types/type.d.js
+++ Tic-Tec-Toe/src/types/type.d.js
@@ -1,7 +1,7 @@
 import { oneOf, arrayOf, shape, number } from "prop-types";
-import { PLAYER_LIST } from "../constants";
+import { PLAYERLIST } from "../constants";
 
-export const OneOfPlayerType = oneOf(PLAYER_LIST);
+export const OneOfPlayerType = oneOf(PLAYERLIST);
 export const oneOfPlayerListType = arrayOf(OneOfPlayerType);
 export const WinnerInfoType = shape({
     winner: OneOfPlayerType,

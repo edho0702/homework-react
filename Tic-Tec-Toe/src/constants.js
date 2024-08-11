605b30410f16391d0ca45eaceb93fb40bfcf9228
dongho <ldd0702@naver.com>
2024-08-11 21:16:21 +0900

    [과제] Tic-Tec-Toe
    - 승리조건 만들기

--- Tic-Tec-Toe/src/constants.js
+++ Tic-Tec-Toe/src/constants.js
@@ -6,3 +6,14 @@ export const PLAYER = {
 export const SQUARES = Array(9).fill(null);
 
 export const COUNT = Object.keys(PLAYER).length;
+
+export const GAMEOVER = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
